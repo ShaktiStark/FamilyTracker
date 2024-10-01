6343f95bd7f05f944034f09fdc23a336275798a9
Your Name <you@example.com>
2024-10-01 20:42:33 +0530

    Normalize line endings

---
+++ solution.js
@@ -0,0 +1,117 @@
+import express from "express";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+// Setup EJS as the view engine
+app.set("view engine", "ejs");
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "school",
+  password: "123456",
+  port: 5432,
+});
+db.connect();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let currentUserId = 1;
+
+let users = [
+  { id: 1, name: "Angela", color: "teal" },
+  { id: 2, name: "Jack", color: "powderblue" },
+];
+
+async function checkVisited() {
+  const result = await db.query(
+    "SELECT country_code FROM visited_countries JOIN users ON users.id = user_id WHERE user_id = $1;",
+    [currentUserId]
+  );
+  let countries = [];
+  result.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+  return countries;
+}
+
+async function getCurrentUser() {
+  const result = await db.query("SELECT * FROM users");
+  users = result.rows;
+  return users.find((user) => user.id == currentUserId);
+}
+
+app.get("/", async (req, res) => {
+  const countries = await checkVisited();
+  const currentUser = await getCurrentUser();
+  res.render("index.ejs", {
+    countries: countries,
+    total: countries.length,
+    users: users,
+    color: currentUser.color,
+  });
+});
+
+app.post("/add", async (req, res) => {
+  const input = req.body["country"];
+  const currentUser = await getCurrentUser();
+
+  try {
+    const result = await db.query(
+      "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
+      [input.toLowerCase()]
+    );
+
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+    try {
+      await db.query(
+        "INSERT INTO visited_countries (country_code, user_id) VALUES ($1, $2)",
+        [countryCode, currentUserId]
+      );
+      res.redirect("/");
+    } catch (err) {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.post("/user", async (req, res) => {
+  if (req.body.add === "new") {
+    res.render("new.ejs");
+  } else {
+    currentUserId = req.body.user;
+    res.redirect("/");
+  }
+});
+
+app.post("/new", async (req, res) => {
+  const name = req.body.name;
+  const color = req.body.color;
+
+  try {
+    const result = await db.query(
+      "INSERT INTO users (name, color) VALUES($1, $2) RETURNING *;",
+      [name, color]
+    );
+
+    const id = result.rows[0].id;
+    currentUserId = id;
+
+    res.redirect("/");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
